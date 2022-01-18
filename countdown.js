f48b9473a00ccd0a2ef3a9c9cbaf6c1f8a3f0c01
Bipul Chaudhary <bipulchau@gmail.com>
2022-01-18 15:09:09 +0545

    Use minimist

--- countdown.js
+++ countdown.js
@@ -1,6 +1,18 @@
 //
 // Collect the time from argv
 //
+const parseArgs = require("minimist");
+const { time } = parseArgs(process.argv);
+
+if (!time) {
+  throw new Error("--time is required");
+}
+
+if (!parseInt(time)) {
+  throw new Error("--time must be a number");
+}
+
+console.log(time);
 
 //
 // Print starts in-line
